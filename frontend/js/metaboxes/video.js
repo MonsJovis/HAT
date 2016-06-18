f1e0b6040d842a422bfa5bae2fdaf0d35afd7e57
MonsJovis <markus.aurich@gmail.com>
2016-06-18 18:26:47 +0200

    read the param tags inside the object with js to obtain the subtitle url and language

--- frontend/js/metaboxes/video.js
+++ frontend/js/metaboxes/video.js
@@ -1,13 +1,35 @@
 (function($) {
 
-	var $videoPlayer, videoObj;
+	var $videoPlayer, videoObj, subtitleFiles = [];
 
 	function initVideoPlayer() {
 
-    $videoPlayer = $('#videoplayer');
-    if ($videoPlayer.length) {
-      videoObj = $('#videoplayer')[0];
-    }
+		$videoPlayer = $('#videoplayer');
+		if ($videoPlayer.length) {
+			videoObj = $('#videoplayer')[0];
+		}
+
+		$('param', $videoPlayer).each(function(index, obj) {
+			var name = $(obj).attr('name'),
+				value = $(obj).attr('value'),
+				subtitlesIndex,
+				subtitleKey;
+			var regEx = /subtitles\[(\d+)\]\[(url|language)\]/g,
+				matches;
+			while ((matches = regEx.exec(name)) !== null) {
+				if (matches.index === regEx.lastIndex) {
+					regEx.lastIndex++;
+				}
+				subtitlesIndex = matches[1];
+				subtitleKey = matches[2];
+			}
+			if (subtitlesIndex !== null) {
+				if (!subtitleFiles[subtitlesIndex]) {
+					subtitleFiles[subtitlesIndex] = {};
+				}
+				subtitleFiles[subtitlesIndex][subtitleKey] = value;
+			}
+		});
 
 		$videoPlayer.on('play', function() {
 			console.log('play');
@@ -39,15 +61,15 @@
 
 		if (videoObj && videoObj.play) {
 			videoObj.play(1);
-      $videoPlayer.trigger('play'); // TODO: trigger does not work
-      renderSubtitles();
+			$videoPlayer.trigger('play'); // TODO: trigger does not work
+			renderSubtitles();
 		}
 
-  }
+	}
 
-  function renderSubtitles() {
-    // TODO: implement the continuos render mechanism for the subtitles
-  }
+	function renderSubtitles() {
+		// TODO: implement the continuos render mechanism for the subtitles
+	}
 
 	$(document).ready(function() {
 		setTimeout(initVideoPlayer, 10);
