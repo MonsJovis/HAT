62eba47554d9a456229253629c0fc9529804a6d8
MonsJovis <markus.aurich@gmail.com>
2016-06-19 19:35:58 +0200

    beautify subtitles.js

--- frontend/js/modules/subtitles.js
+++ frontend/js/modules/subtitles.js
@@ -1,49 +1,41 @@
 function parseSubtitles(data) {
-    var subtitles = [];
+	var subtitles = [];
 	var lines = data.split("\n");
-	var j = 1; //count phrases
+	var j = 1; //phrases counter
 	var phrase = {
-		begin: "",
-		end: "",
-		text: ""
+		begin: null,
+		end: null,
+		text: null
 	};
-	for (var i=2; i<lines.length; i++){ //count lines
-		if (parseInt(lines[i])==j){ //start new phrase
+	for (var i = 2; i < lines.length; i++) { //lines counter
+		if (parseInt(lines[i], 10) == j) { //start new phrase
 			continue;
-		}
-		else if (/\d\d:\d\d:\d\d\.\d\d\d.+/.test(lines[i])) { //push begin and end
+		} else if (/\d\d:\d\d:\d\d\.\d\d\d.+/.test(lines[i])) { //push begin and end
 			lines[i] = lines[i].slice(0, 28);
 			times = lines[i].split(" --> ");
 			phrase.begin = toMilliseconds(times[0]);
 			phrase.end = toMilliseconds(times[1]);
-			continue;
-		}
-		else if ((lines[i] == "\r") || (lines[i] == "\n") || (lines[i] == "") ){ //push phrase to subtitles
+		} else if ((lines[i] === "\r") || (lines[i] === "\n") || (lines[i] === "")) { //push phrase to subtitles
 			subtitles.push(phrase);
-			j = j+1;
+			j = j + 1;
 			phrase = {
-				begin: "",
-				end: "",
-				text: ""
+				begin: null,
+				end: null,
+				text: null
 			};
-			continue;
-		}
-		else { //push text
-	 		if (phrase.text == ""){
+		} else { //push text
+			if (phrase.text === null) {
 				phrase.text = lines[i];
-			}
-			else
-			{
+			} else {
 				phrase.text = phrase.text + "\n" + lines[i];
-
 			}
-			continue;
 		}
 	}
-return subtitles;
+	return subtitles;
 }
-function toMilliseconds(time){
-    var firstsplit = time.split(":");
-    var secondsplit = firstsplit[2].split(".");
-    return parseInt(firstsplit[0]) * 3600000 + parseInt(firstsplit[1]) * 60000 + parseInt(secondsplit[0]) * 1000 + parseInt(secondsplit[1]);
+
+function toMilliseconds(time) {
+	var firstsplit = time.split(":");
+	var secondsplit = firstsplit[2].split(".");
+	return parseInt(firstsplit[0], 10) * 3600000 + parseInt(firstsplit[1], 10) * 60000 + parseInt(secondsplit[0], 10) * 1000 + parseInt(secondsplit[1], 10);
 }
