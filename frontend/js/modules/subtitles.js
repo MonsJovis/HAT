e2bd7b672f681d5703026565b12ef727c7e6c7ff
Uliana Sirotina <sirotina.u@yandex.ru>
2016-06-19 19:10:02 +0200

    fixed the bug with empty line, now works for all webvtt files

--- frontend/js/modules/subtitles.js
+++ frontend/js/modules/subtitles.js
@@ -1,32 +1,47 @@
 function parseSubtitles(data) {
     var subtitles = [];
-    var phrases = data.split("\n\n");
-    var lines = [];
-    var times = [];
-    for (var i=1; i<phrases.length; i++){
-        var phrase = {
-            begin: "",
-            end: "",
-            text: ""
-        };
-        lines = phrases[i].split("\n");
-        times = lines[1].split(" --> ");
-        phrase.begin = toMilliseconds(times[0]);
-        phrase.end = toMilliseconds(times[1]);
-        for (var j=2; j<lines.length; j++){ //counts lines of text
-            if (phrase.text == ""){
-                phrase.text = lines[j];
-            }
-            else
-            {
-                phrase.text = phrase.text + "\n" + lines[j];
-            }
-        }
-        subtitles.push(phrase);
-    }
-    return subtitles;
-}
+	var lines = data.split("\n");
+	var j = 1; //count phrases
+	var phrase = {
+		begin: "",
+		end: "",
+		text: ""
+	};
+	for (var i=2; i<lines.length; i++){ //count lines
+		if (parseInt(lines[i])==j){ //start new phrase
+			continue;
+		}
+		else if (/\d\d:\d\d:\d\d\.\d\d\d.+/.test(lines[i])) { //push begin and end
+			lines[i] = lines[i].slice(0, 28);
+			times = lines[i].split(" --> ");
+			phrase.begin = toMilliseconds(times[0]);
+			phrase.end = toMilliseconds(times[1]);
+			continue;
+		}
+		else if ((lines[i] == "\r") || (lines[i] == "\n") || (lines[i] == "") ){ //push phrase to subtitles
+			subtitles.push(phrase);
+			j = j+1;
+			phrase = {
+				begin: "",
+				end: "",
+				text: ""
+			};
+			continue;
+		}
+		else { //push text
+	 		if (phrase.text == ""){
+				phrase.text = lines[i];
+			}
+			else
+			{
+				phrase.text = phrase.text + "\n" + lines[i];
 
+			}
+			continue;
+		}
+	}
+return subtitles;
+}
 function toMilliseconds(time){
     var firstsplit = time.split(":");
     var secondsplit = firstsplit[2].split(".");
