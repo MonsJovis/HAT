52e5f97e4a7bc11233f07e4d742da0d9ec19870b
MonsJovis <markus.aurich@gmail.com>
2016-07-04 17:33:54 +0200

    more debug msg

--- frontend/js/metaboxes/video.js
+++ frontend/js/metaboxes/video.js
@@ -255,6 +255,8 @@
 	}
 
 	function setSubtitleTimeout(subtitle) {
+    debug('videoObj.playPosition: ' + videoObj.playPosition);
+    debug('videoObj.currentTime: ' + videoObj.currentTime);
 		var currentTime = isFireHbb ? videoObj.playPosition : videoObj.currentTime * 1000,
       showttl = subtitle.begin - currentTime,
 			hidettl = subtitle.end - currentTime;
